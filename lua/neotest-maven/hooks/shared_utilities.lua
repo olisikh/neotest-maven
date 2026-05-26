271d64f0e384779df6de13d894fef0d167447ffa
Oleksii Lisikh <olisikh@ebay.com>
2026-05-26 21:58:13 +0200

    fix(hooks): look for package until found or EOL

--- lua/neotest-maven/hooks/shared_utilities.lua
+++ lua/neotest-maven/hooks/shared_utilities.lua
@@ -1,10 +1,14 @@
 local lib = require("neotest.lib")
 
--- TODO: How to improve using Treesitter or similar?
 --- @param file_path string
 local function get_package_name(file_path)
-	local first_line = lib.files.read_lines(file_path)[1]
-	return (first_line:gsub("^package ", ""):gsub(";", ""))
+	for _, line in ipairs(lib.files.read_lines(file_path)) do
+		local package = line:match("^package ([^;]+);")
+		if package then
+			return package
+		end
+	end
+	return ""
 end
 
 return {
